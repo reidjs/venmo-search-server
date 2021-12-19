49fbd1f3699d04caa9d5cc218fd4805c97df295c
reidjs <reidsherman@gmail.com>
2021-12-19 10:34:08 -0500

    first commit :

---
+++ server.js
@@ -0,0 +1,71 @@
+const MongoClient = require("mongodb").MongoClient;
+const cors = require('cors')
+const express = require('express')
+require('dotenv').config()
+const port = process.env.PORT || 3080
+
+const app = express()
+const assert = require("assert");
+
+const corsOptions = {
+  origin: ['http://localhost:8000', "TODO: PUT PROD URL HERE!"],
+  optionsSuccessStatus: 200 
+}
+
+const createAggregate = search => {
+  const agg = [
+    {
+      $search: {
+        text: {
+          // searches for "query" in the "path" field
+          query: search,
+          path: "note"
+        },
+      },
+    },
+    {
+      $limit: 10,
+    },
+    {
+      // to limit reponse to certain files 
+      // https://docs.mongodb.com/manual/reference/operator/aggregation/project/
+      $project: {
+        _id: 0,
+        "payment.target.user.username": 1,
+        note: 1,
+      },
+    },
+  ];
+  return agg
+}
+
+app.get('/', cors(corsOptions), (req, res) => {
+  const queryParams = req.query
+  const search =  queryParams.q
+  if (!search) {
+    res.json([])
+    return
+  }
+  MongoClient.connect(
+    `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.abbj1.mongodb.net/`,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    async function (connectErr, client) {
+      assert.equal(null, connectErr);
+      dbClient = client
+      const coll = client.db("test").collection("venmo");
+      const agg = createAggregate(search)
+      let cursor = await coll.aggregate(agg);
+      const results = []
+      await cursor.forEach((doc) => {
+        results.push(doc)
+      });
+      res.json(results)
+      client.close();
+    }
+  );
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
